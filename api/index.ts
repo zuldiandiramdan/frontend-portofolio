fa4f5fe0001d0f7663aa3d1244687f828280e1f2
zuldiandi <tblog77@gmail.com>
2024-08-11 12:23:34 +0800

    React Add

--- api/index.ts
+++ api/index.ts
@@ -3,12 +3,17 @@ import path from "path";
 
 const app = express();
 
+app.use(
+  "/static-react/",
+  express.static(path.join(__dirname, "react", "react-frontend", "out"))
+);
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "express", "index.html"));
 });
 
-app.get('/react', (req, res) => {
-    res.sendFile(path.join(__dirname, "react", "index.html"));
+app.get("/react", (req, res) => {
+  res.sendFile(path.join(__dirname, "react", "react-frontend", "out", "index.html"));
 });
 
 module.exports = app;
---
+++ api/react/react-frontend/next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    basePath: '/static-react'
+};
+
+export default nextConfig;
